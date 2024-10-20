433defcefe0bfb15e50ee46db8db18a53a225e56
Sathish Srinivasan Radhakrishnan <ssrinivasanr@csr.com.au>
2024-10-20 22:17:42 +1100

    update cypress config to support users data load from CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,33 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    baseUrl: 'https://www.saucedemo.com',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+    e2e: {
+        baseUrl: "https://www.saucedemo.com",
+        setupNodeEvents(on, config) {
+            // Load any environment variables from process.env (CI)
+            const usersFromEnv = {
+                standard: {
+                    username:
+                        process.env.STANDARD_USERNAME ||
+                        config.env.users?.standard.username,
+                    password:
+                        process.env.STANDARD_PASSWORD ||
+                        config.env.users?.standard.password
+                },
+                lockedOut: {
+                    username:
+                        process.env.LOCKEDOUT_USERNAME ||
+                        config.env.users?.lockedOut.username,
+                    password:
+                        process.env.LOCKEDOUT_PASSWORD ||
+                        config.env.users?.lockedOut.password
+                }
+            };
+
+            // Merge the usersFromEnv with the existing config.env (from cypress.env.json)
+            config.env.users = usersFromEnv;
+
+            return config;
+        }
+    }
 });
