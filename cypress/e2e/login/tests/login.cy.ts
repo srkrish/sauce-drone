9e335fbff0acbf4411f8ee314d86e572011489d1
Sathish Srinivasan Radhakrishnan <ssrinivasanr@csr.com.au>
2024-10-20 23:17:17 +1100

    <one more exclusive test>

--- cypress/e2e/login/tests/login.cy.ts
+++ cypress/e2e/login/tests/login.cy.ts
@@ -35,7 +35,7 @@ describe("Login form", () => {
         LoginPage.validateLogin(lockedOutUser.username, lockedOutUser.password);
     });
 
-    it("routes to inventory page for a standard user", () => {
+    it.only("routes to inventory page for a standard user", () => {
         // enter the standard user's username and password
         LoginPage.login(standardUser.username, standardUser.password);
     });
