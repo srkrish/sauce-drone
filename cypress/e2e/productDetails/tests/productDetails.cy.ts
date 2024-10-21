386cf798895ea5fd2d26ce641d1e759af3b7b99f
Sathish Srinivasan Radhakrishnan <ssrinivasanr@csr.com.au>
2024-10-22 00:51:24 +1100

    <refactor to align with the class implementation>

--- cypress/e2e/productDetails/tests/productDetails.cy.ts
+++ cypress/e2e/productDetails/tests/productDetails.cy.ts
@@ -3,8 +3,9 @@ import { ProductDetailsPage } from "../page/productDetails.page";
 import items from "../../../fixtures/inventory.json";
 
 describe("Product details", () => {
-    // define the standard and locked out users
     const standardUser = Cypress.env("users").standard;
+    const productDetailsPage = new ProductDetailsPage(); // Create an instance of ProductDetailsPage
+
     beforeEach(() => {
         cy.visit("/");
         LoginPage.login(standardUser.username, standardUser.password);
@@ -14,15 +15,15 @@ describe("Product details", () => {
         // go to the inventory page
         cy.visit("/inventory.html");
         // validate the product list with the items from the fixture
-        ProductDetailsPage.validateProductDetails(items);
+        productDetailsPage.validateProductDetails(items); // Use the instance
     });
 
     it("validates add product to the cart", () => {
         // go to the inventory page
         cy.visit("/inventory.html");
-        // validate the product list with the items from the fixture
-        ProductDetailsPage.validateAddToCart(items[0].name);
-        ProductDetailsPage.getCart()
+        // validate adding the first product to the cart
+        productDetailsPage.validateAddToCart(items[0].name); // Use the instance
+        productDetailsPage.getCart()
             .find(".shopping_cart_badge")
             .scrollIntoView()
             .should("have.text", "1");
@@ -31,14 +32,14 @@ describe("Product details", () => {
     it("validates remove product from the cart", () => {
         // go to the inventory page
         cy.visit("/inventory.html");
-        // validate the product list with the items from the fixture
-        ProductDetailsPage.validateAddToCart(items[0].name);
-        ProductDetailsPage.getCart()
+        // validate adding and removing the first product from the cart
+        productDetailsPage.validateAddToCart(items[0].name); // Use the instance
+        productDetailsPage.getCart()
             .find(".shopping_cart_badge")
             .scrollIntoView()
             .should("have.text", "1");
-        ProductDetailsPage.getRemoveFromCart().click();
-        ProductDetailsPage.getCart()
+        productDetailsPage.getRemoveFromCart().click();
+        productDetailsPage.getCart()
             .find(".shopping_cart_badge")
             .should("not.exist");
     });
